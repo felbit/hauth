f58f70f1471be727a3545d0eb8224093bbd440bc
Martin Penckert <martin.penckert@gmail.com>
2019-10-26 17:30:04 +0200

    Finishes the in memory database for authentication

--- src/Adapter/InMemory/Auth.hs
+++ src/Adapter/InMemory/Auth.hs
@@ -20,11 +20,13 @@ state change in place. That obviously needs a Monad.
 module Adapter.InMemory.Auth where
 
 import           ClassyPrelude
-import qualified Domain.Auth                   as D
+import           Control.Monad.Except
 import           Data.Has
+import qualified Domain.Auth                   as D
+import           Text.StringRandom
 
 data State = State
-  { stateAuth :: [ (D.UserId, D.Auth) ]
+  { stateAuths :: [ (D.UserId, D.Auth) ]
   , stateUnverifiedEmails :: Map D.VerificationCode D.Email
   , stateVerifiedEmails :: Set D.Email
   , stateUserIdCounter :: Int
@@ -41,7 +43,7 @@ data State = State
 type InMemory r m = (Has (TVar State) r, MonadReader r m, MonadIO m)
 
 initialState :: State
-initialState = State { stateAuth             = []
+initialState = State { stateAuths            = []
                      , stateUnverifiedEmails = mempty
                      , stateVerifiedEmails   = mempty
                      , stateUserIdCounter    = 0
@@ -49,31 +51,132 @@ initialState = State { stateAuth             = []
                      , stateSessions         = mempty
                      }
 
--- ** Repository functions (copied from Domain.Auth) with InMemory constraint.
+-- ** Auth Repository implementation under InMemory constraint.
 
+-- | Generates a new verification code (random 16 character string) similar to
+-- 'newSession' and checks if the given email is a duplicate (returns
+-- RegistrationErrorEmailTaken error in that case). If email is unique it is
+-- added to stateAuths with a newly generated UserId (increments the
+-- userId-counter). Adds the email and generated verification token to
+-- stateUnverifiedEmails before returning with the verification code.
 addAuth
   :: InMemory r m => D.Auth -> m (Either D.RegistrationError D.VerificationCode)
-addAuth = undefined
+addAuth auth = do
+  tvar  <- asks getter
 
+  -- generate the verification code
+  vCode <- liftIO $ stringRandomIO "[A-Za-z0-9]{16}"
+
+  atomically . runExceptT $ do
+    state <- lift $ readTVar tvar
+
+    -- check, whether the given email is a duplicate
+    let auths       = stateAuths state
+        email       = D.authEmail auth
+        isDuplicate = any (email ==) . map (D.authEmail . snd) $ auths
+    when isDuplicate $ throwError D.RegistrationErrorEmailTaken
+
+    -- update state
+    let newUserId      = stateUserIdCounter state + 1
+        newAuths       = (newUserId, auth) : auths
+        unverifieds    = stateUnverifiedEmails state
+        newUnverifieds = insertMap vCode email unverifieds
+        newState       = state { stateAuths            = newAuths
+                               , stateUserIdCounter    = newUserId
+                               , stateUnverifiedEmails = newUnverifieds
+                               }
+    lift $ writeTVar tvar newState
+    return vCode
+
+-- | Looks up an Email in stateUnverifiedEmails via given VerificationCode and
+-- moves that into stateVerifiedEmails. If the given VerificationCode does not
+-- map to any Email it throws an EmailVerificationErrorInvalidCode error.
 setEmailAsVerified
   :: InMemory r m
   => D.VerificationCode
   -> m (Either D.EmailVerificationError ())
-setEmailAsVerified = undefined
+setEmailAsVerified vCode = do
+  tvar <- asks getter
+  atomically . runExceptT $ do
+    state <- lift $ readTVar tvar
+    let unverifieds = stateUnverifiedEmails state
+        verifieds   = stateVerifiedEmails state
+        maybeEmail  = lookup vCode unverifieds
+    case maybeEmail of
+      Nothing    -> throwError D.EmailVerificationErrorInvalidCode
+      Just email -> do
+        let newUnverifieds = deleteMap vCode unverifieds
+            newVerifieds   = insertSet email verifieds
+            newState       = state { stateUnverifiedEmails = newUnverifieds
+                                   , stateVerifiedEmails   = newVerifieds
+                                   }
+        lift $ writeTVar tvar newState
 
+-- | Extracts the UserId from the Auth (return Nothing if no UserId is found).
+-- With UserId check for verified Emails and return UserId and the current
+-- verification status.
 findUserByAuth :: InMemory r m => D.Auth -> m (Maybe (D.UserId, Bool))
-findUserByAuth = undefined
+findUserByAuth auth = do
+  tvar  <- asks getter
+  state <- liftIO $ readTVarIO tvar
+  let maybeUserId = map fst . find ((auth ==) . snd) $ stateAuths state
+  case maybeUserId of
+    Nothing  -> return Nothing
+    Just uId -> do
+      let verifieds  = stateVerifiedEmails state
+          email      = D.authEmail auth
+          isVerified = email `elem` verifieds
+      return $ Just (uId, isVerified)
 
 findEmailFromUserId :: InMemory r m => D.UserId -> m (Maybe D.Email)
-findEmailFromUserId = undefined
+findEmailFromUserId uId = do
+  tvar  <- asks getter
+  state <- liftIO $ readTVarIO tvar
+  let maybeAuth = map snd . find ((uId ==) . fst) $ stateAuths state
+  return $ D.authEmail <$> maybeAuth
 
+-- ** Session Repository implementation under InMemory constraint.
+
+-- | Generates a unique random 16 chars string as "SessionId" and adds the
+-- sessionId mapped to the given userId to in memory state database.
+-- Adding uId on front of the sessionId string ensures a truly unique sessionId.
 newSession :: InMemory r m => D.UserId -> m D.SessionId
-newSession = undefined
+newSession uId = do
+  tvar <- asks getter
+  sId  <- liftIO $ ((tshow uId) <>) <$> stringRandomIO "[A-Za-z0-9]{16}"
+  atomically $ do
+    state <- readTVar tvar
+    let sessions    = stateSessions state
+        newSessions = insertMap sId uId sessions
+        newState    = state { stateSessions = newSessions }
+    writeTVar tvar newState
+    return sId
 
+-- | Looks for sessionId in the stateSessions structure.
 findUserIdBySessionId :: InMemory r m => D.SessionId -> m (Maybe D.UserId)
 findUserIdBySessionId sId = do
   tvar <- asks getter
   liftIO $ lookup sId . stateSessions <$> readTVarIO tvar
 
+-- ** Notification implementation under InMemory constraint.
+
+-- | Inserts a given email address and the corresponding "VerificationCode" to
+-- state's stateNotifications
 notifyEmailVerification :: InMemory r m => D.Email -> D.VerificationCode -> m ()
-notifyEmailVerification = undefined
+notifyEmailVerification email vCode = do
+  tvar <- asks getter
+  atomically $ do
+    state <- readTVar tvar
+    let notifications    = stateNotifications state
+        newNotifications = insertMap email vCode notifications
+        newState         = state { stateNotifications = newNotifications }
+    writeTVar tvar newState
+
+-- | For testing purposes this function fakes the notification process since I
+-- am not able to send actual email out (yet).
+getNotificationsForEmail
+  :: InMemory r m => D.Email -> m (Maybe D.VerificationCode)
+getNotificationsForEmail email = do
+  tvar  <- asks getter
+  state <- liftIO $ readTVarIO tvar
+  return $ lookup email $ stateNotifications state
